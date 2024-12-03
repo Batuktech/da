a24b38f12b2d81654f1d5e22d7b0bd24a9904539
Batuktech <batuukka@gmail.com>
2024-12-03 18:47:58 +0800

    Oov

---
+++ phase2.d
@@ -0,0 +1,10 @@
+
+phase2.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	68 38 26 40 00       	push   $0x402638
+   5:	48 c7 c7 9d 04 33 27 	mov    $0x2733049d,%rdi
+   c:	c3                   	ret    
---
+++ phase2.s
@@ -0,0 +1,3 @@
+pushq $0x00402638
+mov $0x2733049d, %rdi
+ret
